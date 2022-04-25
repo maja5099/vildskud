e856cd768e4d6ac995c528799be4e004f6c2b779
Maja Sørensen <majabirchsorensen@gmail.com>
2022-04-25 11:12:23 +0200

    roboto font tilføjet

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
       },
       fontFamily: {
         'anton': "'Anton', sans-serif",
+        'roboto': "'Roboto', sans-serif",
       },
       backgroundImage: {
         // 'heroBg': "url('/img/bg.jpg')",
