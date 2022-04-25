c87a06e452f584fd54ef84f59b3b42afbe516357
Maja Sørensen <majabirchsorensen@gmail.com>
2022-04-25 10:43:26 +0200

    farver tilføjet

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: "#C6EE56",
+        purple: "#DAB7F9",
+        red: "#F2443B",
+        black: "#000000",
+      },
+      fontFamily: {
+        // 'sheffield': "'mrsheffield-pro', sans-serif",
+      },
+      backgroundImage: {
+        // 'heroBg': "url('/img/bg.jpg')",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
